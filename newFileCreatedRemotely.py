a8a9b19e295e594f7e6cd1d990b5c8376afb2cac
Xiaocan <hsiaotsan.li@mail.utoronto.ca>
2020-09-18 17:50:31 +0800

    commit using pycharm. try #1.

--- newFileCreatedRemotely.py
+++ newFileCreatedRemotely.py
@@ -1,2 +1,3 @@
 print("This file is created remotey using www.github.com")
 print("This line is created remotely using github. 2020.9.18.17.37")
+print("This line is created locally using pyycharm. 2020.9.18.17.49")
